72d16bd60e633239a0ed625ee47bd029f90023be
Di-Is <hommageMe@gmail.com>
2025-03-24 21:36:15 +0900

    change: mise alias

--- dot_zsh.d/mise.zsh
+++ dot_zsh.d/mise.zsh
@@ -3,8 +3,9 @@
 eval "$(mise completion zsh)"
 
 # alias
-alias me='mise'
-alias mer='mise run'
-alias met='mise task'
-alias mei='mise install'
-alias mel='mise list'
+alias m='mise'
+alias t='mise task'
+alias mr='mise run'
+alias mt='mise task'
+alias mi='mise install'
+alias ml='mise list'
--- dot_zsh.d/task.zsh
+++
@@ -1,5 +0,0 @@
-# aliases
-alias t="task"
-
-# shell completion
-eval "$(task --completion zsh)"
