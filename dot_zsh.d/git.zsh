46de0f11ede608eb39c1d2c683786b427ffabeb7
Di-Is <hommageMe@gmail.com>
2025-03-27 23:26:53 +0900

    add: fazzy git checkout

--- dot_zsh.d/git.zsh
+++ dot_zsh.d/git.zsh
@@ -1,3 +1,10 @@
 
 alias gp="git push"
 alias gf="git fetch"
+
+fco() {
+  local branches branch
+  branches=$(git --no-pager branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git switch $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
