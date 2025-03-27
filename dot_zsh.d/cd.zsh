1c83a84d3c5206d690a652cdb00424eb084bfc38
Di-Is <hommageMe@gmail.com>
2025-03-27 23:24:27 +0900

    add: fazzy cd

--- dot_zsh.d/cd.zsh
+++ dot_zsh.d/cd.zsh
@@ -4,3 +4,8 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 
 alias root='cd "$(git rev-parse --show-toplevel)"'
+
+fcd(){
+    local dir
+    dir=$(fd --type d . ${1:-.} --hidden 2> /dev/null | fzf +m) && cd "$dir"
+} 
