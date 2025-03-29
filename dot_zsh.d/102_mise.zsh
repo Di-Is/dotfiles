ebad6d7670746bdeabbb9e3301196c0feb6a7937
Di-Is <hommageMe@gmail.com>
2025-03-29 22:14:07 +0900

    refactor

--- dot_zsh.d/102_mise.zsh
+++ dot_zsh.d/102_mise.zsh
@@ -2,15 +2,15 @@
 # activation
 _evalcache mise activate zsh
 
-# shell completion
-on_demand_completion "mise" "mise completion zsh"
-
 # alias
 alias m='mise'
 alias mr='mise run'
 alias mt='mise task'
 alias mi='mise install'
 alias ml='mise list'
+
+# shell completion
+on_demand_completion "mise" "mise completion zsh"
 
 t() {
   if [ $# -eq 0 ]; then
