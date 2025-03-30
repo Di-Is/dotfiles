86027190de9d588b3dcebf0c56a0db7e3269ac97
Di-Is <hommageMe@gmail.com>
2025-03-30 15:46:43 +0900

    refactor

---
+++ dot_zsh.d/000_source.zsh
@@ -0,0 +1,14 @@
+# zcompile
+# Ref. https://zenn.dev/fuzmare/articles/zsh-source-zcompile-all
+function source {
+  ensure_zcompiled $1
+  builtin source $1
+}
+function ensure_zcompiled {
+  local compiled="$1.zwc"
+  if [[ ! -r "$compiled" || "$1" -nt "$compiled" ]]; then
+    echo "Compiling $1"
+    zcompile $1
+  fi
+}
+ensure_zcompiled ~/.zshrc
