83e765b46a0ff73cc37d73628aa6b2cf615c55df
Di-Is <rhoxbox@gmail.com>
2025-03-30 15:39:01 +0900

    fix

--- dot_zsh.d/docker.zsh
+++ dot_zsh.d/docker.zsh
@@ -10,6 +10,6 @@ alias dcu="docker compose up"
 alias dcd="docker compose down"
 
 # shell completion
-if ! command -v docker >/dev/null 2>&1; then
+if command -v docker >/dev/null 2>&1; then
     on_demand_completion docker
 fi
