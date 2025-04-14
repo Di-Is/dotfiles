9e89a198012291343ba2a1851e8c500d87f4a45c
Di-Is <hommageMe@gmail.com>
2025-04-14 21:01:54 +0900

    change: modify eza alias

--- dot_zsh.d/eza.zsh
+++ dot_zsh.d/eza.zsh
@@ -4,9 +4,6 @@ alias la='eza --all'
 alias ll='eza --all --group --header --group-directories-first --long'
 alias lg='eza --group --header --group-directories-first --long --git --git-ignore'
 alias le='eza --group --header --group-directories-first --long --extended'
-# WARNING: override lt cmd
-alias lt='eza --group --header --group-directories-first --tree --level LEVEL'
-# WARNING: override tree cmd
-alias tree='eza --group --header --group-directories-first --tree'
+alias ltr='eza --group --header --group-directories-first --tree --level LEVEL'
 alias lc='eza --group --header --group-directories-first --across'
 alias lo='eza --group --header --group-directories-first --oneline'
