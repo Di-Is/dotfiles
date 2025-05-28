97634382752497b624e969b0cb0a87875e134fb8
Di-Is <hommageMe@gmail.com>
2025-05-28 20:56:10 +0900

    add: ruff alias

---
+++ dot_zsh.d/ruff.zsh
@@ -0,0 +1,8 @@
+# aliases
+alias rfc="ruff check"
+alias rff="ruff format"
+alias rfr="ruff rule"
+alias rfa="ruff analyze"
+
+# shell completion
+on_demand_completion "ruff" "ruff generate-shell-completion zsh"
