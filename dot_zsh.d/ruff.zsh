8c2e7b9e25815be791ec9e14b64a584d555ba145
Di-Is <hommageMe@gmail.com>
2025-10-18 14:29:40 +0900

    add: ruff check statistics alias

--- dot_zsh.d/ruff.zsh
+++ dot_zsh.d/ruff.zsh
@@ -3,6 +3,7 @@ alias rfc="ruff check"
 alias rff="ruff format"
 alias rfr="ruff rule"
 alias rfa="ruff analyze"
+alias rfcs="ruff check --statistics"
 
 # shell completion
 on_demand_completion "ruff" "ruff generate-shell-completion zsh"
